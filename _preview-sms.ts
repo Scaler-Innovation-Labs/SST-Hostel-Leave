fa23d7dae5194c846112b3554d3548b6930694cd
NEERASA-VEDA-VARSHIT <n.vedvarshit@gmail.com>
2026-06-16 20:49:00 +0530

    chore: add reseed and preview-sms utility scripts

---
+++ _preview-sms.ts
@@ -0,0 +1,9 @@
+import { db } from "@/lib/db";
+import { leaveRequests } from "@/db/schema/leave";
+import { eq } from "drizzle-orm";
+
+async function main() {
+  const leave = await db.select().from(leaveRequests).where(eq(leaveRequests.id, "44bb3672-ce59-4621-980d-5cd41e376b71")).then(r => r[0]);
+  console.log(JSON.stringify(leave, null, 2));
+}
+main();
